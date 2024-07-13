badcbf6c06d75c7c6f53bc5ddcd6fba21352ff7e
patthrills <asubiaropatrick@gmail.com>
2024-07-13 07:37:44 +0100

    Change front page UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,7 +73,7 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       backgroundImage:{
-        "hero": "url('/images/banner.jpeg')"
+        "hero": "url('/images/banner.svg')"
       }
     },
   },
